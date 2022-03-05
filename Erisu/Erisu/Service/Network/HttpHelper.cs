7480c36df97ea9af9b8c4545edbd1644685dfbb1
worrix64 <1738435753@qq.com>
2022-03-05 13:00:14 +0800

    新增HttpHelper

---
+++ Erisu/Erisu/Service/Network/HttpHelper.cs
@@ -0,0 +1,48 @@
+﻿using Erisu.Model;
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Erisu.Service.Network;
+
+public class HttpHelper
+{
+    public static int BufferSize { get; set; } = 1024 * 1024;
+
+    public static readonly HttpClient HttpClient;
+
+    static HttpHelper() => HttpClient = new HttpClient();
+
+    /// <summary>
+    /// GET Method
+    /// </summary>
+    /// <param name="url"></param>
+    /// <param name="authorization"></param>
+    /// <param name="httpCompletionOption"></param>
+    /// <returns></returns>
+    public static async Task<HttpResponseMessage> HttpGetAsync(string url, Tuple<string, string> authorization = default, HttpCompletionOption httpCompletionOption = HttpCompletionOption.ResponseContentRead)
+    {
+        using var requestMessage = new HttpRequestMessage(HttpMethod.Get, url);
+
+        if (authorization != null) 
+            requestMessage.Headers.Authorization = new AuthenticationHeaderValue(authorization.Item1, authorization.Item2);
+
+        var responseMessage = await HttpClient.SendAsync(requestMessage, httpCompletionOption, CancellationToken.None);
+
+        if (responseMessage.StatusCode.Equals(HttpStatusCode.Found))
+        {
+            string redirectUrl = responseMessage.Headers.Location.AbsoluteUri;
+
+            responseMessage.Dispose();
+            GC.Collect();
+
+            return await HttpGetAsync(redirectUrl, authorization, httpCompletionOption);
+        }
+
+        return responseMessage;
+    }
+}
