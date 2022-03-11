2d910c8166ed5a0dc0b057b5ac647d5e84045b41
worrix64 <1738435753@qq.com>
2022-03-11 18:04:17 +0800

    完善各类下载API

---
+++ Erisu/Erisu/Service/Network/API/BaseApi.cs
@@ -0,0 +1,26 @@
+﻿using Erisu.Model;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace Erisu.Service.Network.API;
+
+public abstract class BaseApi
+{
+    public string Url { get; set; }
+
+    public string VersionsManifest { get; set; }
+
+    public string Assets { get; set; }
+
+    public string Libraries { get; set; }
+
+    public virtual async Task<VersionManifestModel> GetVersionManifest()
+    {
+        using var res = await HttpHelper.HttpGetAsync(this.VersionsManifest);
+
+        return JsonConvert.DeserializeObject<VersionManifestModel>(await res.Content.ReadAsStringAsync());
+    }
+}
+
+
+
---
+++ Erisu/Erisu/Service/Network/API/BmclApi.cs
@@ -0,0 +1,34 @@
+﻿using Erisu.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Erisu.Service.Network.API;
+
+public class BmclApi : BaseApi
+{
+    public BmclApi()
+    {
+        Url = "https://bmclapi2.bangbang93.com";
+        VersionsManifest = $"{Url}/mc/game/version_manifest.json";
+        Assets = $"{Url}/assets";
+        Libraries = $"{Url}/maven";
+    }
+
+    public override async Task<VersionManifestModel> GetVersionManifest()
+    {
+        using var res = await HttpHelper.HttpGetAsync(this.VersionsManifest);
+        var model = Newtonsoft.Json.JsonConvert.DeserializeObject<VersionManifestModel>(await res.Content.ReadAsStringAsync());
+        
+        var list = model.Versions.ToList();
+
+        for (int i = 0; i < list.Count; i++)
+            list[i].Url = list[i].Url.Replace("https://launchermeta.mojang.com", Url);
+
+        model.Versions = list;
+        return model;
+    }
+}
+
