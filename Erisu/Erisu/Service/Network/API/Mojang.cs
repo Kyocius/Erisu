c78058d9c3afe1f6419679741fd00b7bf84f3e2e
worrix64 <1738435753@qq.com>
2022-03-12 16:33:40 +0800

    新增MojangApi

---
+++ Erisu/Erisu/Service/Network/API/Mojang.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Erisu.Service.Network.API;
+
+public class Mojang : BaseApi
+{
+    public Mojang()
+    {
+        Url = "https://launcher.mojang.com";
+        VersionManifest = "http://launchermeta.mojang.com/mc/game/version_manifest.json";
+        Assets = "http://resources.download.minecraft.net";
+        Libraries = "https://libraries.minecraft.net";
+    }
+}
