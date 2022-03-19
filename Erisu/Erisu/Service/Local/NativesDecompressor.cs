c21338b237341f75c371f1fbaa63ba283392f4c9
Yoroion <1738435753@qq.com>
2022-03-19 21:21:55 +0800

    修改README，增加新Model

---
+++ Erisu/Erisu/Service/Local/NativesDecompressor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Erisu.Service.Local;
+
+public class NativesDecompressor
+{
+    public string Root { get; set; }
+    public string Id { get; set; }
+    
+    public NativesDecompressor(string root, string id)
+    {
+        Root = root; 
+        Id = id;
+    }
+
+    public void Decompress()
+    {
+        //TODO(待实现 Native Model)
+    }
+}
