9e3ca00caeaea4f84d0be2dbb3b3b21a5b996719
Yoroion <1738435753@qq.com>
2022-03-20 13:14:43 +0800

    新增UUIDHelper

---
+++ Erisu/Erisu/Service/Local/UUIDHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace Erisu.Service.Local;
+
+public class UUIDHelper
+{
+    public static Guid FromString(string input)
+    {
+        using var md5 = MD5.Create();
+        return new Guid(md5.ComputeHash(Encoding.UTF8.GetBytes(input)));
+    }
+}
+
