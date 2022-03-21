a2c1670b46be9c0db5678963bb3ac786abb39f34
Yoroion <1738435753@qq.com>
2022-03-21 13:27:59 +0800

    新增IDependence接口

---
+++ Erisu/Erisu/Interface/IDependence.cs
@@ -0,0 +1,27 @@
+﻿using Erisu.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Erisu.Interface;
+
+/// <summary>
+/// 游戏依赖接口 
+/// </summary>
+public interface IDependence
+{
+    /// <summary>
+    ///  获取对应游戏依赖的下载请求
+    /// </summary>
+    /// <param name="root">.minecraft游戏目录</param>
+    /// <returns></returns>
+    HttpDownloadRequest GetDownloadRequest(string root);
+
+    /// <summary>
+    ///  获取游戏以来相对于.minecraft目录的路径
+    /// </summary>
+    /// <returns></returns>
+    string GetRelativePath();
+}
